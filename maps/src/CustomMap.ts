7b0496de180e13e4051bbd5cbb4c366f123360db
holomdev <holom.dev@gmail.com>
2020-12-26 23:01:08 -0600

    Implicit type Checks

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,3 @@
-import { User } from "./User";
-import { Company } from "./Company";
-
 interface Mappable {
   location: {
     lat: number;
@@ -30,14 +27,4 @@ export class CustomMap {
       },
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng,
-  //     },
-  //   });
-  // }
 }
