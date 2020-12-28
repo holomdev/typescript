e85ed6a928d84b6ee770959c0f2ae9cca0c00427
holomdev <holom.dev@gmail.com>
2020-12-28 16:18:57 -0600

    Updating Interface Definitions

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,19 +1,28 @@
-import * as faker from 'faker';
+import * as faker from "faker";
 
 export class Company {
   companyName: string;
   catchPhrase: string;
   location: {
-    lat: number,
-    lng: number
-  }
+    lat: number;
+    lng: number;
+  };
 
   constructor() {
     this.companyName = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
-      lng: parseFloat(faker.address.longitude())
+      lng: parseFloat(faker.address.longitude()),
     };
+  }
+
+  markerContent(): string {
+    return `
+      <div>
+        <h1>Company name: ${this.companyName}</h1>
+        <h3>Catchphrase: ${this.catchPhrase}</h3>
+      </div>
+    `;
   }
 }
