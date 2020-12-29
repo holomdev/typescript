add348465bdcb9bbba27e21fe4ab003053ceba36
holomdev <holom.dev@gmail.com>
2020-12-29 16:37:04 -0600

    typescript is really smart

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,11 +7,14 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
+                //if collection is an array of numbers
                 if (this.collection[i] > this.collection[j + 1]) {
                     var leftHand = this.collection[j];
                     this.collection[j] = this.collection[j + 1];
                     this.collection[j + 1] = leftHand;
                 }
+                //if collection is a string, do this instead
+                // logic to compare and swap characters in a string
             }
         }
     };
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,17 +1,23 @@
 class Sorter {
 
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string) {
   }
 
   sort(): void {
     const {length} = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
+
+        //if collection is an array of numbers
         if (this.collection[i] > this.collection[j + 1]) {
           const leftHand = this.collection[j];
           this.collection[j] = this.collection[j + 1];
           this.collection[j + 1] = leftHand;
         }
+
+        //if collection is a string, do this instead
+        // logic to compare and swap characters in a string
+
       }
     }
   }
