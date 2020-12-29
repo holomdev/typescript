6f2cd21a52426950fc1b72119823eab463131cf1
holomdev <holom.dev@gmail.com>
2020-12-29 17:50:01 -0600

    Extracting key logic

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,32 +1,4 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                //All of this only works if collection is number[]
-                //if collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    //collection === number[]
-                    if (this.collection[i] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                //Only going to work if collection is string
-                //if collection is a string, do this instead
-                // logic to compare and swap characters in a string
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter);
