e7c935f64e0118123dbba90557103310f73fb1dd
holomdev <holom.dev@gmail.com>
2020-12-29 17:16:55 -0600

    type guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,14 +7,21 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
+                //All of this only works if collection is number[]
                 //if collection is an array of numbers
-                if (this.collection[i] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                if (this.collection instanceof Array) {
+                    //collection === number[]
+                    if (this.collection[i] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
                 }
+                //Only going to work if collection is string
                 //if collection is a string, do this instead
                 // logic to compare and swap characters in a string
+                if (typeof this.collection === 'string') {
+                }
             }
         }
     };
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -8,15 +8,23 @@ class Sorter {
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
 
+        //All of this only works if collection is number[]
         //if collection is an array of numbers
-        if (this.collection[i] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.collection instanceof Array) {
+          //collection === number[]
+          if (this.collection[i] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
 
+        //Only going to work if collection is string
         //if collection is a string, do this instead
         // logic to compare and swap characters in a string
+        if (typeof this.collection === 'string') {
+
+        }
 
       }
     }
