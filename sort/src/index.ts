678a91088a2d0d8cca0be3ee88faf39a236ca533
holomdev <holom.dev@gmail.com>
2020-12-30 09:59:07 -0600

    Sorting Arbitrary Collections

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
+import {CharactersCollection} from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, -52]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('bdgatAAcvzaxP');
+const charactersSorter = new Sorter(charactersCollection);
+charactersSorter.sort();
+console.log(charactersCollection.data);
