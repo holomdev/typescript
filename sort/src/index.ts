bb74c14994c9f57d18f51e3cfc6d01faacd01b5d
holomdev <holom.dev@gmail.com>
2021-01-04 22:55:14 -0600

    Just One More fix

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection';
+import {LinkedList} from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, -52]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,14 @@ const charactersCollection = new CharactersCollection('bdgatAAcvzaxP');
 const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 console.log(charactersCollection.data);
+
+const linkList = new LinkedList();
+linkList.add(500);
+linkList.add(-10);
+linkList.add(-3);
+linkList.add(4);
+
+const listSorter = new Sorter(linkList);
+listSorter.sort();
+linkList.print();
+
